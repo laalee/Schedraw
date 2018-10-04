83d8530f6afeaa233388f21876304437aebb3973
HsinYuLee <laalee0525@gmail.com>
2018-10-04 14:07:55 +0800

    Create category tableview cells.

--- Schedule/Schedule/Category/CategoryViewController.swift
+++ Schedule/Schedule/Category/CategoryViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class CategoryViewController: UIViewController {
 
+    @IBOutlet weak var categoryTableView: UITableView!
+
+    let identifiers = [
+        String(describing: CategoryTitleTableViewCell.self),
+        String(describing: ColorTableViewCell.self),
+        String(describing: DeleteTableViewCell.self)
+    ]
+    
     var eventType: EventType?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTableView()
     }
 
     // MARK: Initialization
@@ -35,6 +44,20 @@ class CategoryViewController: UIViewController {
         return viewController
     }
 
+    func setupTableView() {
+
+        categoryTableView.dataSource = self
+
+        categoryTableView.delegate = self
+
+        for identifier in identifiers {
+
+            categoryTableView.register(
+                UINib(nibName: identifier, bundle: nil),
+                forCellReuseIdentifier: identifier)
+        }
+    }
+
     @IBAction func saveCategory(_ sender: Any) {
 
         dismiss(animated: true, completion: nil)
@@ -44,5 +67,46 @@ class CategoryViewController: UIViewController {
 
         dismiss(animated: true, completion: nil)
     }
+
+}
+
+extension CategoryViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+
+        return identifiers.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifiers[indexPath.section], for: indexPath)
+
+        switch indexPath.section {
+
+        case 0:
+            guard let titleCell = cell as? CategoryTitleTableViewCell else { return cell }
+
+            return titleCell
+
+        case 1:
+            guard let colorCell = cell as? ColorTableViewCell else { return cell }
+
+            return colorCell
+
+        default:
+            guard let deleteCell = cell as? DeleteTableViewCell else { return cell }
+
+            return deleteCell
+        }
+    }
+
+}
+
+extension CategoryViewController: UITableViewDelegate {
 
 }
